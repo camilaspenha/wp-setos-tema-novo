38e3f2f2351a89349f23218114c3504ad37010d4
Camila Silva da Penha <camilaspenha@gmail.com>
2024-07-18 17:35:45 -0300

    style: contato

--- patterns/page-contato-template.php
+++ patterns/page-contato-template.php
@@ -13,28 +13,32 @@
   <div class="container" data-aos="fade-up">
 
     <div class="row mt-5">
-      <div class="col-lg-4">
+      <div class="col-lg-5">
         <div class="section-title">
         <h2>Contato</h2>
-      </div>
-        <div class="info">
-          <div class="email">
-            <i class="bi bi-envelope-at"></i>
-            <h4>Email:</h4>
-            <a class="ms-3" href="mailto:<?= get_theme_mod('email') ?>" title="Link para Enviar um E-mail"><?= get_theme_mod('email') ?></a>
-          </div>
+        </div>
+          <div class="info">
+            <div class="email d-md-flex">
+              <i class="bi bi-envelope-at me-md-3"></i>
+              <div>
+                <h4>Email:</h4>
+                <a href="mailto:<?= get_theme_mod('email') ?>" title="Link para Enviar um E-mail"><?= get_theme_mod('email') ?></a>
+              </div>
+            </div>
 
-          <div class="phone">
-            <i class="bi bi-whatsapp"></i>
-            <h4>Whatsapp:</h4>            
-            <a class="ms-3" href="<?= get_theme_mod('whatsapp-api') ?>" target="_blank" title="Abre Página Externa - Whatsapp Web">
-            <?= get_theme_mod('whatsapp'); ?>
-            </a>
+            <div class="phone d-md-flex">
+              <i class="bi bi-whatsapp me-md-3"></i>
+              <div>
+                <h4>Whatsapp:</h4>            
+                <a href="<?= get_theme_mod('whatsapp-api') ?>" target="_blank" title="Abre Página Externa - Whatsapp Web">
+                <?= get_theme_mod('whatsapp'); ?>
+              </div>
+              </a>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="col-lg-8 mt-5 mt-lg-0">
+      <div class="col-lg-7 p-0 p-md-3 mt-3 mt-lg-0">
       <?php get_template_part( 'parts/template-parts/form');   ?>
       </div>
     </div>
